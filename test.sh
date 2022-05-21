ad1b58e4f59b154ad06ca647bd821f8284e97e7b
John G <legah2000@gmail.com>
2022-05-21 09:08:32 -0400

    dev committ

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 testing application
 operation DevOps=150,000 x2
-I will  make it by the will and power and grace of Gos that flows through me
+I will  make it by the will and power and grace of God that flows through me
